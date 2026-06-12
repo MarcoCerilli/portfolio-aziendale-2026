6685cb2392c9de03c8f18e4a0dac2a135d4cfe8f
Marco Cerilli <cerillimarco15@gmail.com>
2026-06-12 13:02:03 +0200

    risolto problema  sezione  progetti

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -49,7 +49,7 @@ export default function ProjectsGrid() {
         layout
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8"
       >
-        <AnimatePresence mode="popLayout">
+        <AnimatePresence>
           {filteredProjects.map((project) => (
             <motion.div
               key={project.title}
